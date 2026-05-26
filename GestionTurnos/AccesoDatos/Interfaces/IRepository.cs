aef67f18f450b6f0c8051b82555c71fc71ea472a
Johnatan López <jhonatan12316@hotmail.com>
2026-05-25 21:01:26 -0500

    FEAT: Se crea IRepository para obligar a clases Truno y Usuario implementar métodos internos.

---
+++ GestionTurnos/AccesoDatos/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace SistemaTurnos.AccesoDatos.Interfaces
+{
+    public interface IRepository<T>
+    {
+        bool Insertar(T obj);
+        bool Actualizar(T obj);
+        DataTable ObtenerTodos();
+    }
+}
