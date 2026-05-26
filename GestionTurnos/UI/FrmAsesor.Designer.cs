a6b646a763c7ca179b63db516911c52502572679
Johnatan López <jhonatan12316@hotmail.com>
2026-05-25 20:45:00 -0500

    FEAT: Se modifica form FrmAsesor para el manejo de turnos, llamado, cancelación y finalización de los mismos

--- GestionTurnos/UI/FrmAsesor.Designer.cs
+++ GestionTurnos/UI/FrmAsesor.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace GestionTurnos.UI
+﻿namespace SistemaTurnos.Presentacion
 {
     partial class FrmAsesor
     {
@@ -28,11 +28,9 @@
             this.btnLogOut = new System.Windows.Forms.Button();
             this.lblColaEspera = new System.Windows.Forms.Label();
             this.pnlTurnoActual = new System.Windows.Forms.Panel();
-
             ((System.ComponentModel.ISupportInitialize)(this.dgvColaEspera)).BeginInit();
             this.pnlTurnoActual.SuspendLayout();
             this.SuspendLayout();
-
             // 
             // lblTituloAsesor
             // 
@@ -40,43 +38,57 @@
             this.lblTituloAsesor.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold);
             this.lblTituloAsesor.Location = new System.Drawing.Point(12, 15);
             this.lblTituloAsesor.Name = "lblTituloAsesor";
-            this.lblTituloAsesor.Size = new System.Drawing.Size(260, 32);
+            this.lblTituloAsesor.Size = new System.Drawing.Size(255, 32);
+            this.lblTituloAsesor.TabIndex = 0;
             this.lblTituloAsesor.Text = "PANEL DE ATENCIÓN";
-
             // 
-            // btnLogOut
+            // lblTurnoActual
             // 
-            this.btnLogOut.Location = new System.Drawing.Point(680, 15);
-            this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(100, 30);
-            this.btnLogOut.Text = "Cerrar Sesión";
-
-            // Panel de Turno Actual
-            this.pnlTurnoActual.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlTurnoActual.Controls.Add(this.lblNumTurno);
-            this.pnlTurnoActual.Controls.Add(this.lblTurnoActual);
-            this.pnlTurnoActual.Controls.Add(this.lblInfoCliente);
-            this.pnlTurnoActual.Location = new System.Drawing.Point(18, 70);
-            this.pnlTurnoActual.Name = "pnlTurnoActual";
-            this.pnlTurnoActual.Size = new System.Drawing.Size(350, 180);
-
             this.lblTurnoActual.AutoSize = true;
             this.lblTurnoActual.Font = new System.Drawing.Font("Segoe UI", 12F);
             this.lblTurnoActual.Location = new System.Drawing.Point(15, 15);
+            this.lblTurnoActual.Name = "lblTurnoActual";
+            this.lblTurnoActual.Size = new System.Drawing.Size(140, 21);
+            this.lblTurnoActual.TabIndex = 0;
             this.lblTurnoActual.Text = "Turno en Atención:";
-
-            this.lblNumTurno.AutoSize = true;
-            this.lblNumTurno.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold);
+            // 
+            // lblNumTurno
+            // 
+            this.lblNumTurno.Font = new System.Drawing.Font("Segoe UI", 42F, System.Drawing.FontStyle.Bold);
             this.lblNumTurno.ForeColor = System.Drawing.Color.DarkBlue;
-            this.lblNumTurno.Location = new System.Drawing.Point(100, 45);
+            this.lblNumTurno.Location = new System.Drawing.Point(15, 45);
+            this.lblNumTurno.Name = "lblNumTurno";
+            this.lblNumTurno.Size = new System.Drawing.Size(320, 75);
+            this.lblNumTurno.TabIndex = 1;
             this.lblNumTurno.Text = "--";
-
+            this.lblNumTurno.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblInfoCliente
+            // 
             this.lblInfoCliente.AutoSize = true;
             this.lblInfoCliente.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Italic);
             this.lblInfoCliente.Location = new System.Drawing.Point(15, 140);
+            this.lblInfoCliente.Name = "lblInfoCliente";
+            this.lblInfoCliente.Size = new System.Drawing.Size(130, 19);
+            this.lblInfoCliente.TabIndex = 2;
             this.lblInfoCliente.Text = "Cliente: Sin asignar";
-
-            // Botones de Acción
+            // 
+            // dgvColaEspera
+            // 
+            this.dgvColaEspera.AllowUserToAddRows = false;
+            this.dgvColaEspera.AllowUserToDeleteRows = false;
+            this.dgvColaEspera.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvColaEspera.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvColaEspera.Location = new System.Drawing.Point(400, 95);
+            this.dgvColaEspera.MultiSelect = false;
+            this.dgvColaEspera.Name = "dgvColaEspera";
+            this.dgvColaEspera.ReadOnly = true;
+            this.dgvColaEspera.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvColaEspera.Size = new System.Drawing.Size(727, 325);
+            this.dgvColaEspera.TabIndex = 7;
+            // 
+            // btnLlamarSiguiente
+            // 
             this.btnLlamarSiguiente.BackColor = System.Drawing.Color.RoyalBlue;
             this.btnLlamarSiguiente.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLlamarSiguiente.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
@@ -84,40 +96,77 @@
             this.btnLlamarSiguiente.Location = new System.Drawing.Point(18, 270);
             this.btnLlamarSiguiente.Name = "btnLlamarSiguiente";
             this.btnLlamarSiguiente.Size = new System.Drawing.Size(350, 45);
+            this.btnLlamarSiguiente.TabIndex = 1;
             this.btnLlamarSiguiente.Text = "LLAMAR SIGUIENTE TURNO";
-
+            this.btnLlamarSiguiente.UseVisualStyleBackColor = false;
+            this.btnLlamarSiguiente.Click += new System.EventHandler(this.btnLlamarSiguiente_Click);
+            // 
+            // btnFinalizarTurno
+            // 
             this.btnFinalizarTurno.BackColor = System.Drawing.Color.MediumSeaGreen;
             this.btnFinalizarTurno.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnFinalizarTurno.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnFinalizarTurno.ForeColor = System.Drawing.Color.White;
             this.btnFinalizarTurno.Location = new System.Drawing.Point(18, 325);
             this.btnFinalizarTurno.Name = "btnFinalizarTurno";
             this.btnFinalizarTurno.Size = new System.Drawing.Size(170, 40);
+            this.btnFinalizarTurno.TabIndex = 2;
             this.btnFinalizarTurno.Text = "Finalizar Atención";
-
+            this.btnFinalizarTurno.UseVisualStyleBackColor = false;
+            this.btnFinalizarTurno.Click += new System.EventHandler(this.btnFinalizarTurno_Click);
+            // 
+            // btnNoSePresento
+            // 
             this.btnNoSePresento.BackColor = System.Drawing.Color.Tomato;
             this.btnNoSePresento.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNoSePresento.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
             this.btnNoSePresento.ForeColor = System.Drawing.Color.White;
             this.btnNoSePresento.Location = new System.Drawing.Point(198, 325);
             this.btnNoSePresento.Name = "btnNoSePresento";
             this.btnNoSePresento.Size = new System.Drawing.Size(170, 40);
+            this.btnNoSePresento.TabIndex = 3;
             this.btnNoSePresento.Text = "No se presentó";
-
-            // Tabla de Cola de Espera
+            this.btnNoSePresento.UseVisualStyleBackColor = false;
+            this.btnNoSePresento.Click += new System.EventHandler(this.btnNoSePresento_Click);
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.BackColor = System.Drawing.Color.LightGray;
+            this.btnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogOut.Location = new System.Drawing.Point(680, 15);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(100, 30);
+            this.btnLogOut.TabIndex = 4;
+            this.btnLogOut.Text = "Cerrar Sesión";
+            this.btnLogOut.UseVisualStyleBackColor = false;
+            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
+            // 
+            // lblColaEspera
+            // 
             this.lblColaEspera.AutoSize = true;
             this.lblColaEspera.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
             this.lblColaEspera.Location = new System.Drawing.Point(400, 70);
+            this.lblColaEspera.Name = "lblColaEspera";
+            this.lblColaEspera.Size = new System.Drawing.Size(194, 19);
+            this.lblColaEspera.TabIndex = 6;
             this.lblColaEspera.Text = "Cola de Espera (Siguientes):";
-
-            this.dgvColaEspera.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvColaEspera.Location = new System.Drawing.Point(400, 95);
-            this.dgvColaEspera.Name = "dgvColaEspera";
-            this.dgvColaEspera.ReadOnly = true;
-            this.dgvColaEspera.Size = new System.Drawing.Size(380, 325);
-
-            // FrmAsesor Config
+            // 
+            // pnlTurnoActual
+            // 
+            this.pnlTurnoActual.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlTurnoActual.Controls.Add(this.lblNumTurno);
+            this.pnlTurnoActual.Controls.Add(this.lblTurnoActual);
+            this.pnlTurnoActual.Controls.Add(this.lblInfoCliente);
+            this.pnlTurnoActual.Location = new System.Drawing.Point(18, 70);
+            this.pnlTurnoActual.Name = "pnlTurnoActual";
+            this.pnlTurnoActual.Size = new System.Drawing.Size(350, 180);
+            this.pnlTurnoActual.TabIndex = 5;
+            // 
+            // FrmAsesor
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1139, 450);
             this.Controls.Add(this.lblColaEspera);
             this.Controls.Add(this.dgvColaEspera);
             this.Controls.Add(this.btnNoSePresento);
@@ -131,12 +180,13 @@
             this.Name = "FrmAsesor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Sistema de Turnos - Panel del Asesor";
-
+            this.Load += new System.EventHandler(this.FrmAsesor_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvColaEspera)).EndInit();
             this.pnlTurnoActual.ResumeLayout(false);
             this.pnlTurnoActual.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
--- GestionTurnos/UI/FrmAsesor.cs
+++ GestionTurnos/UI/FrmAsesor.cs
@@ -1,20 +1,267 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using SistemaTurnos.AccesoDatos.Repositorios;
+using SistemaTurnos.Entidades;
+using SistemaTurnos.LogicaNegocio;
 
-namespace GestionTurnos.UI
+namespace SistemaTurnos.Presentacion
 {
     public partial class FrmAsesor : Form
     {
-        public FrmAsesor()
+        public FrmAsesor() : this(0) { }
+
+        private readonly TurnoRepository _turnoRepo = new TurnoRepository();
+        private Turno _turnoActual = null;
+        private int _idModuloAsignado;
+
+        public FrmAsesor(int idModulo)
         {
             InitializeComponent();
+            _idModuloAsignado = idModulo;
+        }
+
+        private void FrmAsesor_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                _turnoRepo.MarcarModuloOcupado(_idModuloAsignado);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No se pudo registrar la ocupación del módulo en el servidor: " + ex.Message,
+                                "Falla de Inicialización", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            ActualizarColaEspera();
+
+            // --- SUSCRIPCIÓN AL EVENTO CON PROCESO SEGURO ---
+            SessionManager.GlobalEvents.OnTurnoChanged += Asesor_OnTurnoChanged;
+
+            // Validaciones preventivas de existencia de columnas en el DataGridView
+            if (dgvColaEspera.Columns["id_prioridad"] != null)
+                dgvColaEspera.Columns["id_prioridad"].Visible = false;
+
+            if (dgvColaEspera.Columns.Contains("nombre_prioridad"))
+                dgvColaEspera.Columns["nombre_prioridad"].HeaderText = "Prioridad";
+
+            EstablecerEstadoControles(enAtencion: false);
+        }
+
+        private void Asesor_OnTurnoChanged()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(ActualizarColaEspera));
+            }
+            else
+            {
+                ActualizarColaEspera();
+            }
+        }
+
+        private void ActualizarColaEspera()
+        {
+            try
+            {
+                dgvColaEspera.DataSource = _turnoRepo.ObtenerTurnosEnEspera();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error al actualizar el flujo de la cola de espera: " + ex.Message);
+            }
+        }
+
+        private void EstablecerEstadoControles(bool enAtencion)
+        {
+            btnLlamarSiguiente.Enabled = !enAtencion;
+            btnFinalizarTurno.Enabled = enAtencion;
+            btnNoSePresento.Enabled = enAtencion;
+
+            if (!enAtencion)
+            {
+                lblNumTurno.Text = "--";
+                lblInfoCliente.Text = "Cliente: Sin asignar";
+                _turnoActual = null;
+            }
+        }
+
+        private void btnLlamarSiguiente_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int idUsuario = SessionManager.GetInstance().UsuarioLogueado?.Id ?? 1;
+                int idModulo = _idModuloAsignado;
+
+                DataTable dtTurno = _turnoRepo.AsignarSiguienteEnCola(idModulo, idUsuario);
+
+                // BLINDAJE CRÍTICO: Validar que el DataTable no sea nulo Y que contenga al menos una fila
+                if (dtTurno == null || dtTurno.Rows.Count == 0)
+                {
+                    MessageBox.Show("No hay turnos pendientes en la cola de espera en este momento.", "Cola Vacía", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    EstablecerEstadoControles(enAtencion: false);
+                    return;
+                }
+
+                DataRow row = dtTurno.Rows[0];
+
+                // --- CORRECCIÓN INTEGRAL DE MAPEO DINÁMICO ---
+                _turnoActual = new TurnoConcreto();
+
+                // 1. Mapeo del ID del Turno
+                if (dtTurno.Columns.Contains("id_turno") && row["id_turno"] != DBNull.Value)
+                    _turnoActual.Id = Convert.ToInt32(row["id_turno"]);
+                else
+                    _turnoActual.Id = 0;
+
+                // 2. Mapeo del Código del Turno (Controlando el singular 'codigo_turno' de tu BD)
+                if (dtTurno.Columns.Contains("codigo_turno") && row["codigo_turno"] != DBNull.Value)
+                    _turnoActual.Codigo = row["codigo_turno"].ToString();
+                else if (dtTurno.Columns.Contains("codigo_turnos") && row["codigo_turnos"] != DBNull.Value)
+                    _turnoActual.Codigo = row["codigo_turnos"].ToString();
+                else
+                    _turnoActual.Codigo = "T-000";
+
+                // 3. Mapeo del Nombre del Cliente (Evita el error si la columna no viene del SP)
+                if (dtTurno.Columns.Contains("nombre_cliente") && row["nombre_cliente"] != DBNull.Value)
+                    _turnoActual.NombreCliente = row["nombre_cliente"].ToString();
+                else
+                    _turnoActual.NombreCliente = "Cliente General"; // Valor por defecto seguro
+
+                // 4. Mapeo del Documento del Cliente
+                if (dtTurno.Columns.Contains("documento_cliente") && row["documento_cliente"] != DBNull.Value)
+                    _turnoActual.DocumentoCliente = row["documento_cliente"].ToString();
+                else
+                    _turnoActual.DocumentoCliente = "Sin Documento";
+
+                // Actualizar etiquetas en la interfaz de usuario
+                lblNumTurno.Text = _turnoActual.Codigo;
+                lblInfoCliente.Text = $"Cliente: {_turnoActual.NombreCliente} ({_turnoActual.DocumentoCliente})";
+
+                EstablecerEstadoControles(enAtencion: true);
+                ActualizarColaEspera();
+
+                // --- NOTIFICACIÓN GLOBAL ---
+                SessionManager.GlobalEvents.NotifyTurnoChanged();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al iniciar la atención del siguiente turno: " + ex.Message, "Error de Operación", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnFinalizarTurno_Click(object sender, EventArgs e)
+        {
+            if (_turnoActual == null)
+            {
+                EstablecerEstadoControles(enAtencion: false);
+                return;
+            }
+
+            try
+            {
+                _turnoActual.Estado = "Atendido";
+                _turnoActual.FechaFinAtencion = DateTime.Now;
+
+                _turnoRepo.Actualizar(_turnoActual);
+
+                MessageBox.Show($"Atención del turno {_turnoActual.Codigo} finalizada con éxito.",
+                                "Turno Cerrado", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                EstablecerEstadoControles(enAtencion: false);
+                ActualizarColaEspera();
+
+                // --- NOTIFICACIÓN GLOBAL ---
+                SessionManager.GlobalEvents.NotifyTurnoChanged();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cerrar la atención del cliente en la base de datos: " + ex.Message,
+                                "Error de Persistencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNoSePresento_Click(object sender, EventArgs e)
+        {
+            if (_turnoActual == null)
+            {
+                EstablecerEstadoControles(enAtencion: false);
+                return;
+            }
+
+            var confirmacion = MessageBox.Show($"¿Desea marcar el turno {_turnoActual.Codigo} como 'No Presentado'?",
+                                                "Confirmar Ausencia", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (confirmacion == DialogResult.Yes)
+            {
+                try
+                {
+                    _turnoActual.Estado = "Cancelado";
+                    _turnoActual.FechaFinAtencion = DateTime.Now;
+
+                    _turnoRepo.Actualizar(_turnoActual);
+
+                    EstablecerEstadoControles(enAtencion: false);
+                    ActualizarColaEspera();
+
+                    // --- NOTIFICACIÓN GLOBAL ---
+                    SessionManager.GlobalEvents.NotifyTurnoChanged();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al registrar la inasistencia en el servidor: " + ex.Message,
+                                    "Error de Persistencia", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // --- SOLUCIÓN DE BLOQUEO CON CONTROL DE ATENCIÓN ACTIVA ---
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // BLINDAJE CRÍTICO: Si el asesor tiene un turno colgado e intenta forzar el cierre de la ventana ("X")
+            if (_turnoActual != null)
+            {
+                MessageBox.Show("No puede cerrar la aplicación ni abandonar el puesto si tiene un turno activo en atención.\nPor favor termine el turno actual o márquelo como ausente.",
+                                "Cierre de Seguridad Detenido", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                e.Cancel = true; // Cancela el evento de cierre del formulario por completo
+                return;
+            }
+
+            // Desuscribimos el evento para que no intente actualizar controles en proceso de destrucción
+            SessionManager.GlobalEvents.OnTurnoChanged -= Asesor_OnTurnoChanged;
+
+            try
+            {
+                // Cambia el estado en SQL de forma síncrona
+                _turnoRepo.LiberarModulo(_idModuloAsignado);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error al liberar módulo en cierre: " + ex.Message);
+            }
+
+            base.OnFormClosing(e);
+        }
+
+        private void btnLogOut_Click(object sender, EventArgs e)
+        {
+            if (_turnoActual != null)
+            {
+                MessageBox.Show("No puede abandonar el puesto de atención si tiene un turno activo en el módulo.", "Atención en Curso",
+                                MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var confirmacion = MessageBox.Show("¿Está seguro de que desea cerrar sesión de asesor y salir?",
+                                                "Confirmación de Salida", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (confirmacion == DialogResult.Yes)
+            {
+                SessionManager.GetInstance().Logout();
+
+                // Al invocar Close(), saltará a OnFormClosing donde validará y liberará el módulo en BD
+                this.Close();
+            }
         }
     }
 }
