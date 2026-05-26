79561b9ed68dab7c7d6d4ec943ab020ef16c35a9
Johnatan López <jhonatan12316@hotmail.com>
2026-05-25 20:48:32 -0500

    FEAT: Se modifica clase FrmDashboard para el manejo lógico de la visualización del turno actual en sistema.

--- GestionTurnos/UI/FrmDashboard.Designer.cs
+++ GestionTurnos/UI/FrmDashboard.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace GestionTurnos.UI
+﻿namespace SistemaTurnos.UI
 {
     partial class FrmDashboard
     {
@@ -19,23 +19,23 @@
         {
             this.components = new System.ComponentModel.Container();
             this.pnlUltimoTurno = new System.Windows.Forms.Panel();
-            this.lblLlamadoPrincipal = new System.Windows.Forms.Label();
-            this.lblTurnoGrande = new System.Windows.Forms.Label();
+            this.lblTiempoEspera = new System.Windows.Forms.Label(); // NUEVA ETIQUETA
             this.lblModuloGrande = new System.Windows.Forms.Label();
+            this.lblTurnoGrande = new System.Windows.Forms.Label();
+            this.lblLlamadoPrincipal = new System.Windows.Forms.Label();
             this.dgvHistorialTurnos = new System.Windows.Forms.DataGridView();
             this.lblHistorialTitulo = new System.Windows.Forms.Label();
             this.timerActualizar = new System.Windows.Forms.Timer(this.components);
             this.lblReloj = new System.Windows.Forms.Label();
             this.lblEmpresa = new System.Windows.Forms.Label();
-
-            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorialTurnos)).BeginInit();
             this.pnlUltimoTurno.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorialTurnos)).BeginInit();
             this.SuspendLayout();
-
             // 
             // pnlUltimoTurno
             // 
             this.pnlUltimoTurno.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.pnlUltimoTurno.Controls.Add(this.lblTiempoEspera); // Agregado al contenedor
             this.pnlUltimoTurno.Controls.Add(this.lblModuloGrande);
             this.pnlUltimoTurno.Controls.Add(this.lblTurnoGrande);
             this.pnlUltimoTurno.Controls.Add(this.lblLlamadoPrincipal);
@@ -43,40 +43,57 @@
             this.pnlUltimoTurno.Location = new System.Drawing.Point(0, 0);
             this.pnlUltimoTurno.Name = "pnlUltimoTurno";
             this.pnlUltimoTurno.Size = new System.Drawing.Size(450, 450);
-
+            this.pnlUltimoTurno.TabIndex = 0;
+            // 
+            // lblTiempoEspera
+            // 
+            this.lblTiempoEspera.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Italic);
+            this.lblTiempoEspera.ForeColor = System.Drawing.Color.DarkGray;
+            this.lblTiempoEspera.Location = new System.Drawing.Point(12, 400);
+            this.lblTiempoEspera.Name = "lblTiempoEspera";
+            this.lblTiempoEspera.Size = new System.Drawing.Size(426, 35);
+            this.lblTiempoEspera.TabIndex = 3;
+            this.lblTiempoEspera.Text = "Espera aprox: -- min";
+            this.lblTiempoEspera.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblModuloGrande
+            // 
+            this.lblModuloGrande.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
+            this.lblModuloGrande.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.lblModuloGrande.Location = new System.Drawing.Point(12, 320);
+            this.lblModuloGrande.Name = "lblModuloGrande";
+            this.lblModuloGrande.Size = new System.Drawing.Size(426, 65);
+            this.lblModuloGrande.TabIndex = 2;
+            this.lblModuloGrande.Text = "MÓDULO 1";
+            this.lblModuloGrande.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTurnoGrande
+            // 
+            this.lblTurnoGrande.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTurnoGrande.ForeColor = System.Drawing.Color.Yellow;
+            this.lblTurnoGrande.Location = new System.Drawing.Point(12, 110);
+            this.lblTurnoGrande.Name = "lblTurnoGrande";
+            this.lblTurnoGrande.Size = new System.Drawing.Size(426, 200);
+            this.lblTurnoGrande.TabIndex = 1;
+            this.lblTurnoGrande.Text = "--";
+            this.lblTurnoGrande.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // lblLlamadoPrincipal
+            // 
             this.lblLlamadoPrincipal.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold);
             this.lblLlamadoPrincipal.ForeColor = System.Drawing.Color.White;
             this.lblLlamadoPrincipal.Location = new System.Drawing.Point(12, 50);
+            this.lblLlamadoPrincipal.Name = "lblLlamadoPrincipal";
             this.lblLlamadoPrincipal.Size = new System.Drawing.Size(426, 45);
+            this.lblLlamadoPrincipal.TabIndex = 0;
             this.lblLlamadoPrincipal.Text = "TURNO ACTUAL";
             this.lblLlamadoPrincipal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
-            // lblTurnoGrande (El número del turno)
-            this.lblTurnoGrande.Font = new System.Drawing.Font("Segoe UI", 110F, System.Drawing.FontStyle.Bold);
-            this.lblTurnoGrande.ForeColor = System.Drawing.Color.Yellow;
-            this.lblTurnoGrande.Location = new System.Drawing.Point(12, 110);
-            this.lblTurnoGrande.Size = new System.Drawing.Size(426, 200);
-            this.lblTurnoGrande.Text = "A01";
-            this.lblTurnoGrande.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
-            // lblModuloGrande (Caja/Asesor)
-            this.lblModuloGrande.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold);
-            this.lblModuloGrande.ForeColor = System.Drawing.Color.LightSkyBlue;
-            this.lblModuloGrande.Location = new System.Drawing.Point(12, 330);
-            this.lblModuloGrande.Size = new System.Drawing.Size(426, 65);
-            this.lblModuloGrande.Text = "MÓDULO 1";
-            this.lblModuloGrande.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-
-            // lblHistorialTitulo
-            this.lblHistorialTitulo.AutoSize = true;
-            this.lblHistorialTitulo.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
-            this.lblHistorialTitulo.Location = new System.Drawing.Point(465, 70);
-            this.lblHistorialTitulo.Name = "lblHistorialTitulo";
-            this.lblHistorialTitulo.Size = new System.Drawing.Size(185, 25);
-            this.lblHistorialTitulo.Text = "ÚLTIMOS LLAMADOS";
-
-            // dgvHistorialTurnos (Lista de turnos anteriores)
+            // 
+            // dgvHistorialTurnos
+            // 
+            this.dgvHistorialTurnos.AllowUserToAddRows = false;
+            this.dgvHistorialTurnos.AllowUserToDeleteRows = false;
+            this.dgvHistorialTurnos.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvHistorialTurnos.BackgroundColor = System.Drawing.SystemColors.Control;
             this.dgvHistorialTurnos.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dgvHistorialTurnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -84,28 +101,48 @@
             this.dgvHistorialTurnos.Name = "dgvHistorialTurnos";
             this.dgvHistorialTurnos.ReadOnly = true;
             this.dgvHistorialTurnos.RowHeadersVisible = false;
+            this.dgvHistorialTurnos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgvHistorialTurnos.Size = new System.Drawing.Size(318, 300);
-
+            this.dgvHistorialTurnos.TabIndex = 1;
+            // 
+            // lblHistorialTitulo
+            // 
+            this.lblHistorialTitulo.AutoSize = true;
+            this.lblHistorialTitulo.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblHistorialTitulo.Location = new System.Drawing.Point(465, 70);
+            this.lblHistorialTitulo.Name = "lblHistorialTitulo";
+            this.lblHistorialTitulo.Size = new System.Drawing.Size(203, 25);
+            this.lblHistorialTitulo.TabIndex = 2;
+            this.lblHistorialTitulo.Text = "ÚLTIMOS LLAMADOS";
+            // 
+            // timerActualizar
+            // 
+            this.timerActualizar.Enabled = true;
+            this.timerActualizar.Interval = 5000;
+            this.timerActualizar.Tick += new System.EventHandler(this.timerActualizar_Tick);
+            // 
+            // lblReloj
+            // 
+            this.lblReloj.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblReloj.Location = new System.Drawing.Point(650, 20);
+            this.lblReloj.Name = "lblReloj";
+            this.lblReloj.Size = new System.Drawing.Size(138, 23);
+            this.lblReloj.TabIndex = 4;
+            this.lblReloj.Text = "00:00:00";
+            this.lblReloj.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
             // lblEmpresa
+            // 
             this.lblEmpresa.AutoSize = true;
             this.lblEmpresa.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic);
             this.lblEmpresa.Location = new System.Drawing.Point(466, 20);
             this.lblEmpresa.Name = "lblEmpresa";
-            this.lblEmpresa.Size = new System.Drawing.Size(135, 21);
+            this.lblEmpresa.Size = new System.Drawing.Size(136, 21);
+            this.lblEmpresa.TabIndex = 3;
             this.lblEmpresa.Text = "Sistema de Turnos";
-
-            // lblReloj
-            this.lblReloj.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
-            this.lblReloj.Location = new System.Drawing.Point(650, 20);
-            this.lblReloj.Size = new System.Drawing.Size(138, 23);
-            this.lblReloj.Text = "00:00:00";
-            this.lblReloj.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-
-            // timerActualizar (Cada 5-10 segundos para refrescar la DB)
-            this.timerActualizar.Enabled = true;
-            this.timerActualizar.Interval = 5000;
-
+            // 
             // FrmDashboard
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
@@ -114,16 +151,17 @@
             this.Controls.Add(this.lblHistorialTitulo);
             this.Controls.Add(this.dgvHistorialTurnos);
             this.Controls.Add(this.pnlUltimoTurno);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None; // Pantalla completa sugerida
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmDashboard";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Monitor de Turnos";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-
-            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorialTurnos)).EndInit();
+            this.Load += new System.EventHandler(this.FrmDashboard_Load);
             this.pnlUltimoTurno.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHistorialTurnos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
@@ -137,5 +175,6 @@
         private System.Windows.Forms.Timer timerActualizar;
         private System.Windows.Forms.Label lblReloj;
         private System.Windows.Forms.Label lblEmpresa;
+        private System.Windows.Forms.Label lblTiempoEspera; // DECLARACIÓN DE LA NUEVA ETIQUETA
     }
 }
--- GestionTurnos/UI/FrmDashboard.cs
+++ GestionTurnos/UI/FrmDashboard.cs
@@ -1,20 +1,274 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using SistemaTurnos.AccesoDatos.Repositorios;
+using SistemaTurnos.LogicaNegocio;
 
-namespace GestionTurnos.UI
+namespace SistemaTurnos.UI
 {
     public partial class FrmDashboard : Form
     {
+        private readonly TurnoRepository _turnoRepo = new TurnoRepository();
+        // Constante diferencial: estimación de 4 minutos estándar de atención promedio por persona
+        private const int TIEMPO_PROMEDIO_MINUTOS = 4;
+
+        // --- NUEVAS VARIABLES DE ESTADO PARA EL TIEMPO DINÁMICO ---
+        private double _tiempoEstimadoDinamico = 0;
+        private int _cantidadEnColaAnterior = -1;
+        private int _modulosActivosAnterior = -1;
+        private DateTime _ultimaVezCalculado = DateTime.Now;
+
         public FrmDashboard()
         {
             InitializeComponent();
+
+            typeof(DataGridView).InvokeMember("DoubleBuffered",
+                System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.SetProperty,
+                null, dgvHistorialTurnos, new object[] { true });
+
+            EstilizarTablaUI();
+
+            SessionManager.GlobalEvents.OnTurnoChanged += Dashboard_OnTurnoChanged;
+        }
+
+        private void EstilizarTablaUI()
+        {
+            Color fondoGrisOscuro = Color.FromArgb(30, 30, 30);
+            Color blancoHueso = Color.FromArgb(245, 247, 250);
+
+            dgvHistorialTurnos.BackgroundColor = blancoHueso;
+            dgvHistorialTurnos.BorderStyle = BorderStyle.None;
+            dgvHistorialTurnos.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dgvHistorialTurnos.GridColor = Color.FromArgb(220, 224, 230);
+            dgvHistorialTurnos.EnableHeadersVisualStyles = false;
+            dgvHistorialTurnos.RowHeadersVisible = false;
+            dgvHistorialTurnos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvHistorialTurnos.MultiSelect = false;
+
+            dgvHistorialTurnos.RowTemplate.Height = 45;
+            dgvHistorialTurnos.ColumnHeadersHeight = 45;
+            dgvHistorialTurnos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            DataGridViewCellStyle headerStyle = new DataGridViewCellStyle();
+            headerStyle.BackColor = fondoGrisOscuro;
+            headerStyle.ForeColor = Color.White;
+            headerStyle.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
+            headerStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvHistorialTurnos.ColumnHeadersDefaultCellStyle = headerStyle;
+
+            DataGridViewCellStyle cellStyle = new DataGridViewCellStyle();
+            cellStyle.BackColor = Color.White;
+            cellStyle.ForeColor = Color.FromArgb(50, 50, 50);
+            cellStyle.Font = new Font("Segoe UI", 14F, FontStyle.Regular);
+            cellStyle.SelectionBackColor = Color.FromArgb(225, 235, 245);
+            cellStyle.SelectionForeColor = Color.FromArgb(30, 30, 30);
+            cellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgvHistorialTurnos.DefaultCellStyle = cellStyle;
+
+            DataGridViewCellStyle alternatingStyle = new DataGridViewCellStyle();
+            alternatingStyle.BackColor = Color.FromArgb(238, 242, 247);
+            dgvHistorialTurnos.AlternatingRowsDefaultCellStyle = alternatingStyle;
+        }
+
+        private void FrmDashboard_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.WindowState = FormWindowState.Normal;
+            this.Size = new Size(1024, 600);
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            lblReloj.Text = DateTime.Now.ToString("hh:mm:ss tt");
+
+            // Asegurar que el Timer esté funcionando para el dinamismo del tiempo
+            if (timerActualizar != null)
+            {
+                timerActualizar.Enabled = true;
+                timerActualizar.Start();
+            }
+
+            ActualizarPantallaTurnos();
+        }
+
+        private void Dashboard_OnTurnoChanged()
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action(ActualizarPantallaTurnos));
+            }
+            else
+            {
+                ActualizarPantallaTurnos();
+            }
+        }
+
+        private void timerActualizar_Tick(object sender, EventArgs e)
+        {
+            lblReloj.Text = DateTime.Now.ToString("hh:mm:ss tt");
+            ActualizarPantallaTurnos(); // Este Tick es el que hará que los minutos cambien en pantalla
+        }
+
+        private void ActualizarPantallaTurnos()
+        {
+            try
+            {
+                DataTable dtTurnos = _turnoRepo.ObtenerTodos();
+
+                if (dtTurnos != null && dtTurnos.Rows.Count > 0)
+                {
+                    // 1. --- ECUACIÓN PREDICTIVA Y COMPORTAMIENTO DINÁMICO ---
+
+                    DataView dvPendientes = new DataView(dtTurnos);
+                    dvPendientes.RowFilter = "Estado = 'Pendiente' OR Estado = 'En Espera'";
+                    int cantidadEnCola = dvPendientes.Count;
+
+                    // CORRECCIÓN: Detectar módulos reales leyendo la BD para captar cuando un asesor entra
+                    DataTable dtModulos = _turnoRepo.ObtenerModulosConfiguracion();
+                    int modulosActivos = 1; // Por defecto 1
+
+                    if (dtModulos != null && dtModulos.Rows.Count > 0)
+                    {
+                        DataView dvMod = new DataView(dtModulos);
+                        // Filtramos los módulos que están operando
+                        dvMod.RowFilter = "estado_modulo = 'Libre' OR estado_modulo = 'Ocupado'";
+                        modulosActivos = Math.Max(1, dvMod.Count);
+                    }
+
+                    // --- LÓGICA DE TIEMPO REAL (DISMINUCIÓN Y AUMENTO) ---
+                    if (cantidadEnCola != _cantidadEnColaAnterior || modulosActivos != _modulosActivosAnterior)
+                    {
+                        // Si entró gente o cambió la cantidad de asesores, recalculamos la base teórica
+                        _tiempoEstimadoDinamico = (double)(cantidadEnCola * TIEMPO_PROMEDIO_MINUTOS) / modulosActivos;
+                        _cantidadEnColaAnterior = cantidadEnCola;
+                        _modulosActivosAnterior = modulosActivos;
+                        _ultimaVezCalculado = DateTime.Now;
+                    }
+                    else
+                    {
+                        // Si la sala está estática, el tiempo estimado para los nuevos disminuye con los minutos
+                        double minutosTranscurridos = (DateTime.Now - _ultimaVezCalculado).TotalMinutes;
+                        _tiempoEstimadoDinamico -= minutosTranscurridos;
+                        _ultimaVezCalculado = DateTime.Now;
+
+                        // Limites lógicos:
+                        if (_tiempoEstimadoDinamico < 1 && cantidadEnCola > 0)
+                            _tiempoEstimadoDinamico = 1; // Si hay gente, mínimo 1 minuto
+                        else if (cantidadEnCola == 0)
+                            _tiempoEstimadoDinamico = 0;
+                    }
+
+                    // Calculamos cuánto tiempo lleva esperando la persona más antigua de la fila (AUMENTA)
+                    int maxMinutosEspera = 0;
+                    if (cantidadEnCola > 0)
+                    {
+                        dvPendientes.Sort = "Id ASC"; // Traer el más viejo
+                        if (dvPendientes[0].Row.Table.Columns.Contains("Fecha"))
+                        {
+                            DateTime fechaCreacion;
+                            if (DateTime.TryParse(dvPendientes[0]["Fecha"].ToString(), out fechaCreacion))
+                            {
+                                maxMinutosEspera = (int)(DateTime.Now - fechaCreacion).TotalMinutes;
+                            }
+                        }
+                    }
+
+                    if (cantidadEnCola > 0)
+                    {
+                        int tiempoEstimadoFinal = (int)Math.Ceiling(_tiempoEstimadoDinamico);
+                        lblTiempoEspera.ForeColor = Color.GreenYellow;
+
+                        string textoInformativo = $"Espera aprox: {tiempoEstimadoFinal} min ({cantidadEnCola} en cola a {modulosActivos} vent.)";
+                        if (maxMinutosEspera > 0)
+                        {
+                            textoInformativo += $" | Máx espera actual: {maxMinutosEspera} min";
+                        }
+
+                        lblTiempoEspera.Text = textoInformativo;
+                    }
+                    else
+                    {
+                        lblTiempoEspera.ForeColor = Color.DarkGray;
+                        lblTiempoEspera.Text = "Sin tiempo de espera (Cola libre)";
+                        _tiempoEstimadoDinamico = 0;
+                    }
+
+                    // 2. PROCESAMIENTO DEL TURNO ACTUAL LLAMADO
+                    DataView dvAtendiendo = new DataView(dtTurnos);
+                    dvAtendiendo.RowFilter = "Estado = 'Llamado' OR Estado = 'Atendiendo'";
+                    dvAtendiendo.Sort = "Id DESC";
+
+                    if (dvAtendiendo.Count > 0)
+                    {
+                        DataRowView primerTurno = dvAtendiendo[0];
+                        string codigoTurno = primerTurno.Row.Table.Columns.Contains("Codigo")
+                            ? primerTurno["Codigo"].ToString()
+                            : primerTurno["codigo_turno"].ToString();
+
+                        string modulo = primerTurno.Row.Table.Columns.Contains("NombreModulo")
+                            ? primerTurno["NombreModulo"].ToString()
+                            : (primerTurno.Row.Table.Columns.Contains("IdModulo") ? $"MÓDULO {primerTurno["IdModulo"]}" : "MÓDULO");
+
+                        if (lblTurnoGrande.Text != codigoTurno)
+                        {
+                            lblTurnoGrande.Text = codigoTurno;
+                            lblModuloGrande.Text = modulo.ToUpper();
+                        }
+                    }
+                    else
+                    {
+                        lblTurnoGrande.Text = "--";
+                        lblModuloGrande.Text = "ESPERANDO";
+                    }
+
+                    // 3. HISTORIAL DE LA GRILLA
+                    DataView dvHistorial = new DataView(dtTurnos);
+                    dvHistorial.RowFilter = "Estado IN ('Atendido', 'Atendiendo', 'Llamado', 'No Presentado', 'Cancelado')";
+                    dvHistorial.Sort = "Id DESC";
+
+                    dgvHistorialTurnos.DataSource = dvHistorial.ToTable();
+                    OcultarColumnasEspecificas();
+                }
+                else
+                {
+                    lblTurnoGrande.Text = "--";
+                    lblModuloGrande.Text = "SALA VACÍA";
+                    lblTiempoEspera.Text = "Espera aprox: 0 min";
+                    _tiempoEstimadoDinamico = 0;
+                    dgvHistorialTurnos.DataSource = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                lblModuloGrande.Text = "ERR: " + ex.Message.Substring(0, Math.Min(15, ex.Message.Length)).ToUpper();
+            }
+        }
+
+        private void OcultarColumnasEspecificas()
+        {
+            try
+            {
+                if (dgvHistorialTurnos.Columns.Count > 0)
+                {
+                    if (dgvHistorialTurnos.Columns.Contains("Id")) dgvHistorialTurnos.Columns["Id"].Visible = false;
+                    if (dgvHistorialTurnos.Columns.Contains("id_turno")) dgvHistorialTurnos.Columns["id_turno"].Visible = false;
+                    if (dgvHistorialTurnos.Columns.Contains("IdPrioridad")) dgvHistorialTurnos.Columns["IdPrioridad"].Visible = false;
+                    if (dgvHistorialTurnos.Columns.Contains("IdUsuarioAsesor")) dgvHistorialTurnos.Columns["IdUsuarioAsesor"].Visible = false;
+                    if (dgvHistorialTurnos.Columns.Contains("DocumentoCliente")) dgvHistorialTurnos.Columns["DocumentoCliente"].Visible = false;
+                    if (dgvHistorialTurnos.Columns.Contains("Fecha")) dgvHistorialTurnos.Columns["Fecha"].Visible = false;
+
+                    if (dgvHistorialTurnos.Columns.Contains("Codigo")) dgvHistorialTurnos.Columns["Codigo"].HeaderText = "Turno";
+                    if (dgvHistorialTurnos.Columns.Contains("codigo_turno")) dgvHistorialTurnos.Columns["codigo_turno"].HeaderText = "Turno";
+                    if (dgvHistorialTurnos.Columns.Contains("Estado")) dgvHistorialTurnos.Columns["Estado"].HeaderText = "Estado";
+                    if (dgvHistorialTurnos.Columns.Contains("NombreModulo")) dgvHistorialTurnos.Columns["NombreModulo"].HeaderText = "Módulo";
+                }
+            }
+            catch { }
+        }
+
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            SessionManager.GlobalEvents.OnTurnoChanged -= Dashboard_OnTurnoChanged;
+            base.OnFormClosed(e);
         }
     }
 }
